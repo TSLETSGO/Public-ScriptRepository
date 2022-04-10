0f99e4ee2ab5c7b0a29d3e4c13179219ba7a1662
TSLETSGO <68672413+TSLETSGO@users.noreply.github.com>
2022-04-10 14:26:21 +0200

    update upgrade sequence

--- PowerShell/Winget/Winget-Upgrade.ps1
+++ PowerShell/Winget/Winget-Upgrade.ps1
@@ -1,2 +1,8 @@
-﻿winget upgrade --all --silent
+﻿write-host "Performing a winget upgrade of all packages:" -ForegroundColor Yellow
+write-host
+winget upgrade --all --silent
+write-host
+write-host "upgrade --include-unknown:" -ForegroundColor Yellow
+write-host
+winget upgrade --all --include-unknown
 pause
